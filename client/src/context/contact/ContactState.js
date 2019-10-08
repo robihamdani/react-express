19d9343b8e590c7bfd3511a1ba4243fdea5c4509
robihamdani <robi4us@gmail.com>
2019-10-09 05:57:09 +0700

    penamban delete

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from "react";
 import axios from "axios";
+import uuid from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -52,8 +53,16 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // add contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+    console.log(addContact);
+  };
 
   // delete contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // set current contact
 
@@ -66,7 +75,9 @@ const ContactState = props => {
   // clear filter
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider
+      value={{ contacts: state.contacts, addContact, deleteContact }}
+    >
       {props.children}
     </ContactContext.Provider>
   );
